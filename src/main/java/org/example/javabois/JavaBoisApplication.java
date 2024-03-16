959fa4035eefc1e15ecc4093f14c5fc41a67bfa7
fuadmahmudzada <fuadmahnudzadabooming@gmail.com>
2024-03-16 14:55:35 +0400

    first

---
+++ src/main/java/org/example/javabois/JavaBoisApplication.java
@@ -0,0 +1,13 @@
+package org.example.javabois;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBoisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaBoisApplication.class, args);
+    }
+
+}
